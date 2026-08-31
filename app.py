db56c81222a157346bcb971ab6a31c2b48fed24e
ShifaJT <sheefa.s@jumbotail.com>
2026-08-31 12:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
+import html
 from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
 from openpyxl.utils import get_column_letter
